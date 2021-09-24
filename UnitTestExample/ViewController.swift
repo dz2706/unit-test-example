11a2ef282acdeeaed59cea00087744e9729c2fb5
Dhwani Shah <dhwanishah@bbpro.local>
2021-09-24 14:47:10 +0530

    file modified

---
+++ UnitTestExample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UnitTestExample
+//
+//  Created by Dhwani Shah on 24/09/21.
+//  Copyright © 2021 dhwani shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func productOfTwoNumbers(num1: Int, num2: Int) -> Int{
+        return num1 + num2
+    }
+
+}
+
